d9d8f6112bb87e0a5efc75b8118a24f418415c7a
Temeines <jordy.detemmerman@hotmail.be>
2017-10-04 16:21:46 +0200

    add notification package

---
+++ config/push-notification.php
@@ -0,0 +1,17 @@
+<?php
+// https://github.com/davibennun/laravel-push-notification
+return array(
+
+    'appNameIOS'     => array(
+        'environment' =>'development',
+        'certificate' =>'/path/to/certificate.pem',
+        'passPhrase'  =>'password',
+        'service'     =>'apns'
+    ),
+    'appNameAndroid' => array(
+        'environment' =>'production',
+        'apiKey'      =>'yourAPIKey',
+        'service'     =>'gcm'
+    )
+
+);
