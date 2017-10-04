331d8d48acb5134314db5efd952ea2fa73b2ce2e
TVke <thomas.verhelst@student.kdg.be>
2017-10-04 13:05:26 +0200

    added TableSeeders

---
+++ database/seeds/GameTableSeeder.php
@@ -0,0 +1,16 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class GameTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+    }
+}
