ab51b6f7e92091380d3de920109fae9252a2bb4f
TVke <thomas.verhelst@student.kdg.be>
2017-10-04 13:53:26 +0200

    added basic html structure

--- resources/views/overzicht.blade.php
+++ resources/views/overzicht.blade.php
@@ -1,4 +1,18 @@
 @extends('layout')
 @section('content')
-	<p>overzicht</p>
+	<form action="{{ route('search') }}" method="post">
+		{{ csrf_field() }}
+		<input name="search" placeholder="memorie">
+		<input type="submit" value="zoek">
+	</form>
+	<h1>
+		<p>GAME</p>
+		<span>changing</span>
+	</h1>
+	<ul>
+		<li>memorie</li>
+	</ul>
+	<footer>
+		&copy; GAMEchanging 2017
+	</footer>
 	@endsection
