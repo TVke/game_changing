08a90c521103430651dc1ec1496825e47f71a0ed
TVke <thomas.verhelst@student.kdg.be>
2017-10-04 12:54:10 +0200

    deleted auth stuff

--- app/Http/Middleware/RedirectIfAuthenticated.php
+++ app/Http/Middleware/RedirectIfAuthenticated.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Http\Middleware;
+namespace GAMEchanging\Http\Middleware;
 
 use Closure;
 use Illuminate\Support\Facades\Auth;
--- config/app.php
+++ config/app.php
@@ -171,11 +171,11 @@ return [
         /*
          * Application Service Providers...
          */
-        App\Providers\AppServiceProvider::class,
-        App\Providers\AuthServiceProvider::class,
-        // App\Providers\BroadcastServiceProvider::class,
-        App\Providers\EventServiceProvider::class,
-        App\Providers\RouteServiceProvider::class,
+        GAMEchanging\Providers\AppServiceProvider::class,
+        GAMEchanging\Providers\AuthServiceProvider::class,
+        // GAMEchanging\Providers\BroadcastServiceProvider::class,
+        GAMEchanging\Providers\EventServiceProvider::class,
+        GAMEchanging\Providers\RouteServiceProvider::class,
 
     ],
 
