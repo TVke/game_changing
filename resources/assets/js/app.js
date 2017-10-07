55fbd32337f8033f0423ba3e4e1712582f6d76b4
TVke <thomas.verhelst@student.kdg.be>
2017-10-07 19:18:05 +0200

    added js to play page and search procistentie

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -6,21 +6,44 @@ var links = document.querySelectorAll("ul li a");
 
 for(var i=0,ilen=links.length;i<ilen;++i){
 	!function(i){
-		// links[i].addEventListener('touchstart',function(){
-		// 	event.preventDefault();
-		// 	links[i].classList.add("hover");
-		// });
 		links[i].addEventListener('click',function(){
 			event.preventDefault();
 			var xhttp = new XMLHttpRequest();
 			xhttp.onreadystatechange = function(){
 				if(this.readyState === 4 && this.status === 200){
-
 					document.body.innerHTML=xhttp.responseText;
-				}
-			};
+					play();
+				}};
 			xhttp.open("GET", links[i].href, true);
 			xhttp.send();
 		});
 	}(i);
 }
+function play(){
+	var countDown = document.getElementById("countDown"),
+		currentTime = countDown.dataset.max,
+		paused = false,
+		pauseButton = document.getElementsByTagName("button")[0],
+
+		timer = setInterval(lower,1000);
+
+	pauseButton.addEventListener("click",function(){
+		event.preventDefault();
+		pauseButton.innerHTML = (!paused)?"speel verder":"pauze";
+		paused = (!paused);
+
+	});
+
+	function lower(){
+		if(!paused){
+			if(currentTime>0){
+				--currentTime;
+				countDown.innerHTML = currentTime;
+			}else if(currentTime=0){
+				clearInterval(timer);
+			}
+		}
+	}
+
+}
+
